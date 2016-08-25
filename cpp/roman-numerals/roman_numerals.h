637c24f1e51d51824780e94bb96d62d4d0e49037
Brendan Creane <bcreane@gmail.com>
2016-08-24 21:44:56 -1000

    roman, a unique solution, I think

---
+++ cpp/roman-numerals/roman_numerals.h
@@ -0,0 +1,56 @@
+#if !defined(ROMAN_NUMERALS_H)
+#define ROMAN_NUMERALS_H
+
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+namespace {
+    string place_value(int d, size_t ndx) {
+
+        vector<vector<string>> numerals = {
+            {"I", "V", "X"},
+            {"X", "L", "C"},
+            {"C", "D", "M"},
+            {"M", "",  ""},
+        };
+
+        if (ndx > 3) ndx = 3;
+        const string& one = numerals[ndx][0];
+        const string& five = numerals[ndx][1];
+        const string& ten = numerals[ndx][2];
+
+        switch (d) {
+            case 1: return one;
+            case 2: return one + one;
+            case 3: return one + one + one;
+            case 4: return one + five;
+            case 5: return five;
+            case 6: return five + one;
+            case 7: return five + one + one;
+            case 8: return five + one + one + one;
+            case 9: return one + ten;
+        }
+
+        return "";
+    }
+}
+
+namespace roman {
+    string convert(int i) {
+        string r;
+        size_t ndx(0);
+        while (i > 0) {
+            int d = i % 10;
+            r.insert(0, place_value(d, ndx));
+            i /= 10;
+            ndx++;
+        }
+
+        return r;
+    }
+}
+
+#endif
